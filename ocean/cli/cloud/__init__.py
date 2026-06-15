b6e1dcd6afa157a355de3e9a6793319b5a0bd991
PlumBlossomMaid <plumblossommaid@github.com>
2026-06-15 15:08:50 +0000

    Fix import order in cloud __init__

--- ocean/cli/cloud/__init__.py
+++ ocean/cli/cloud/__init__.py
@@ -20,13 +20,13 @@ from ocean.cli.cloud.auth import login as _login_cli
 from ocean.cli.cloud.auth import login as _login_fn
 from ocean.cli.cloud.auth import logout as _logout_cli
 from ocean.cli.cloud.auth import logout as _logout_fn
+from ocean.cli.cloud.delete import delete as _delete_cli
+from ocean.cli.cloud.delete import delete_file as _delete_fn
 from ocean.cli.cloud.download import download as _download_cli
 from ocean.cli.cloud.download import download_file
 from ocean.cli.cloud.job import job
 from ocean.cli.cloud.upload import upload as _upload_cli
 from ocean.cli.cloud.upload import upload_file, upload_folder
-from ocean.cli.cloud.delete import delete as _delete_cli
-from ocean.cli.cloud.delete import delete_file as _delete_fn
 
 
 @click.group()
